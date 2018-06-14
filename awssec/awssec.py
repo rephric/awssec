2b711c60e0028714417ec70ca904cb8a2c767d5f
rephric <zjestrella1@gmail.com>
2018-06-13 22:24:23 -0400

    Skeleton Created and TruffleHog functionaility complete, time to work on boto3 and default configs

--- awssec/awssec.py
+++ awssec/awssec.py
@@ -1,15 +1,11 @@
-from truffleHog import truffleHog
-import click
-import boto3
-import os
-import base64
 import re
+import click
+from truffleHog import truffleHog
+import json
 
 
-class AwsAccesskey(click.ParamType):
+class AwsAccessKey(click.ParamType):
     # Search for access key IDs: (?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9]).
-    # In English, this regular expression says: Find me 20-character, uppercase, alphanumeric strings that don’t have
-    # any uppercase, alphanumeric characters immediately before or after.
     name = 'aws_access_key'
 
     def convert(self, value, param, ctx):
@@ -17,18 +13,15 @@ class AwsAccesskey(click.ParamType):
 
         if not found:
             self.fail(
-                f'{value} is not a AWS Access Key IDs',
-                param,
-                ctx,
+                'AWS Access Key is not a AWS Access Key IDs',
+                param
             )
 
         return value
 
 
-class AwsSecretAccesskey(click.ParamType):
+class AwsSecretAccessKey(click.ParamType):
     # Search for secret access keys: (?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=]).
-    # In English, this regular expression says: Find me 40-character, base-64 strings that don’t have any base 64
-    # characters immediately before or after.
     name = 'aws_secret_access_key'
 
     def convert(self, value, param, ctx):
@@ -36,87 +29,76 @@ class AwsSecretAccesskey(click.ParamType):
 
         if not found:
             self.fail(
-                f'{value} is not a AWS Secret Access Key IDs',
-                param,
-                ctx,
+                'AWS Secret Access Key is not a AWS Secret Access Key IDs',
+                param
             )
 
         return value
 
 
-class GitUrl(click.ParamType):
-    # Makes sure the user sent a real github url
-    name = 'git_url'
+def truffle(git_url, do_regex, custom_regex):
+    if do_regex:
+        strings_found = truffleHog.find_strings(git_url=git_url, since_commit=None, max_depth=1000000,
+                                                printJson=True, do_regex=True, do_entropy=True,
+                                                surpress_output=True, custom_regexes=custom_regex)
+    if not do_regex:
+        strings_found = truffleHog.find_strings(git_url=git_url, since_commit=None,
+                                                max_depth=1000000, printJson=True, do_entropy=True,
+                                                surpress_output=True)
+    return strings_found
 
-    def convert(self, value, param, ctx):
-        found = re.match(r'((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?', value)
-
-        if not found:
-            self.fail(
-                f'{value} is not a AWS Secret Access Key IDs',
-                param,
-                ctx,
-            )
-
-        return value
+# def config(aws_access_key, aws_secret_access_key):
 
 
-class AwsSec:
-
-    def __init__(self):
-
-        self.ctx.obj = {}
-
-    @click.group()
-    @click.option('--aws_access_key', '--access', type=AwsAccesskey(), hide_input=True,
-                  help='AWS Access Key ID to interact with the boto3 api.')
-    @click.option('--aws_secret_access_key', '--secret', type=AwsSecretAccesskey(), hide_input=True,
-                  help='AWS Secret Access Key to interact with the boto3 api')
-    @click.option('--git_url', type=GitUrl(), help='The github repository url that is going to be searched')
-    @click.option('--do_regex', is_flag=True, help='Do custom regex searches while scanning the github repository.')
-    @click.option('--custom_regexes', type=str, default='{}', help='Provide the custom regexes to be applied.')
-    @click.pass_context
-    def main(self, ctx, git_url, do_regex, custom_regexes, aws_access_key, aws_secret_access_key):
-        """
-        /b
-           ___                _____
-          / _ \              /  ___|
-         / /_\ \_      _____ \ `--.  ___  ___
-        |  _  \ \ /\ / / __| `--. \/ _ \/ __|
-        | | | |\ V  V /\__ \/\__/ /  __/ (__
-        \_| |_/ \_/\_/ |___/\____/ \___|\___|
+@click.group()
+@click.pass_context
+def main(self):
+    """
+    /b
+        ___                _____
+      / _ \              /  ___|
+     / /_\ \_      _____ \ `--.  ___  ___
+    |  _  \ \ /\ / / __| `--. \/ _ \/ __|
+    | | | |\ V  V /\__ \/\__/ /  __/ (__
+    \_| |_/ \_/\_/ |___/\____/ \___|\___|
+    /b
+    """
+    pass
 
 
-        WORK IN PROGRESS: Calls all the functions at the given time, the orchestrator of the program.
-        """
+@main.command()
+@click.argument('profile')
+@click.option('--aws_access_key', '-a', type=AwsAccessKey(), hide_input=True,
+              help='AWS Access Key ID to interact with the boto3 api.')
+@click.option('--aws_secret_access_key', '-s', type=AwsSecretAccessKey(), hide_input=True,
+              help='AWS Secret Access Key to interact with the boto3 api')
+@click.pass_context
+def aws(self, profile, aws_access_key, aws_secret_access_key):
+    """
+    Stores AWS configuration values in a file
+    """
+    if profile and not aws_secret_access_key:
+        aws_access_key = click.prompt("Please enter your AWS Access Key", type=AwsAccessKey(),
+                                      hide_input=True, default=aws_access_key)
+        aws_secret_access_key = click.prompt("Please enter your AWS Secret Access Key", hide_input=True,
+                                             type=AwsSecretAccessKey(), default=aws_secret_access_key)
+    print(aws_access_key)
+    print(aws_secret_access_key)
+    print(profile)
 
-        # Stores configuration values for AWS keys.
 
-        ctx.obj = {
-            'aws_access_key': aws_access_key,
-            'aws_secret_access_key': aws_secret_access_key,
-            'git_url': git_url,
-            'custom_regexes': custom_regexes,
-            'do_regex': do_regex
+@main.command()
+@click.argument('git_url')
+@click.option('--do_regex', is_flag=True, help='Do custom regex searches while scanning the github repository.')
+@click.option('--custom_regex', type=str, default='{}', help='Provide the custom regex to be applied.')
+@click.pass_context
+def scan(self, git_url, do_regex, custom_regex):
+    """
+    Uses truffleHog to do an entropy test on github repositories.
+    """
+    th = truffle(git_url, do_regex, custom_regex)
+    print(json.dumps(th))
 
-        }
 
-    @main.command()
-    @click.argument('scan_git')
-    @click.pass_context
-    def scan_git(self, ctx, git_url, do_regex, custom_regexes):
-        """ Uses trufflehog to do an entropy test on github repositories."""
-
-        if ctx.obj['do_regex']:
-            strings_found = truffleHog.find_strings(git_url=ctx.obj['git_url'], since_commit=None, max_depth=1000000,
-                                                    printJson=True, do_regex=True, do_entropy=True,
-                                                    surpress_output=True, custom_regexes=ctx.obj['custom_regexes'])
-        elif not ctx.obj['do_regex']:
-                strings_found = truffleHog.find_strings(git_url=ctx.obj['git_url'], since_commit=None,
-                                                        max_depth=1000000, printJson=True, do_entropy=True,
-                                                        surpress_output=True)
-
-        return strings_found()
-
-    if __name__ == '__main__':
-        main()
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ except ImportError:
     from distutils.core import setup
 config = {
         'description': 'AwsSec',
-        'author': 'Zachary Estrella & Trevor Behrens',
+        'author': 'Zachary Estrella',
         'url': 'github.com',
         'download_url': 'github.com',
         'author_email': 'zjestrella1@gmail.com',
         'version': '1.0',
         'install_requires': ['nose', 'Click'],
         'packages': ['boto3', 'trufflehog'],
-        'scripts': [],
+        'entry_points': 'awssec=awssec:main]',
         'name': 'AwsSec'
         }
 
